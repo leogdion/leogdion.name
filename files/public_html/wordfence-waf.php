3a99263b98655e84cb9972405922c58fc32168a5
Leo Dion <leogdion@brightdigit.com>
2023-07-07 15:03:18 +0000

    Setup Docker Image for Old Site

--- files/public_html/wordfence-waf.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
-
-if (file_exists('/home/customer/www/leogdion.name/public_html/wp-content/plugins/wordfence/waf/bootstrap.php')) {
-	define("WFWAF_LOG_PATH", '/home/customer/www/leogdion.name/public_html/wp-content/wflogs/');
-	include_once '/home/customer/www/leogdion.name/public_html/wp-content/plugins/wordfence/waf/bootstrap.php';
-}
-?>
