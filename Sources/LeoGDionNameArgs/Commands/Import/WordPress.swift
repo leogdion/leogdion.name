64c0b2c5045e02fe14573e854e9df8cc5f1e1484
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-07-27 21:31:24 +0000

    Automate image import

--- Sources/LeoGDionNameArgs/Commands/Import/WordPress.swift
+++ Sources/LeoGDionNameArgs/Commands/Import/WordPress.swift
@@ -13,6 +13,9 @@ public extension LeoGDionNameSiteCommand.ImportCommand {
     @Argument(help: "Directory which contains a single or multiple imports.", completion: CompletionKind.directory)
     public var wordpressExportsDirectory: String
 
+    @Option(help: "Directory which contains images, pdf files, and other assets.", completion: CompletionKind.directory)
+    public var importAssetsDirectory: String?
+
     @Option(help: "Base directory for image downloads, redirect files, and other resources.", completion: CompletionKind.directory)
     public var exportResourcesDirectory: String
 
@@ -20,12 +23,12 @@ public extension LeoGDionNameSiteCommand.ImportCommand {
     public var exportMarkdownDirectory: String
 
     @Option(help: "Path to Save Images Relative to Resources.")
-    public var imageRelativePath = "media/wp-images"
+    public var assetRelativePath = "media/wp-assets"
 
-    @Flag(help: "Overwrite Downloaded Images.")
-    public var overwriteImages: Bool = false
+    @Flag(help: "Overwrite Downloaded Assets.")
+    public var overwriteAssets: Bool = false
 
-    @Flag(help: "Skip Downloading Images.")
+    @Flag(help: "Skip Downloading Assets.")
     public var skipDownload: Bool = false
 
     public init() { }
@@ -35,10 +38,8 @@ public extension LeoGDionNameSiteCommand.ImportCommand {
     }
 
     public func run() throws {
-      let processor = try WordPressMarkdownProcessor(postFilters: [
+      var processor = try WordPressMarkdownProcessor(postFilters: [
         RegexKeyPostFilter(pattern: "post", keyPath: \.type),
-        RegexKeyPostFilter(pattern: "^empowerapps-show", keyPath: \.name, not: true),
-        RegexKeyPostFilter(pattern: "^$", keyPath: \.name, not: true),
         RegexKeyPostFilter(pattern: "publish", keyPath: \.status)
       ])
       try processor.begin(withSettings: self)
